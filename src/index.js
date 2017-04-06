3d34ecef3e4d06054ba7e9c77948de6bbff67602
Nicolas Perriault <nicolas@perriault.net>
2017-04-06 12:10:32 +0200

    Add a --json option to render valid JSON.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ program
 program
   .option('-t, --template <template>', 'JSON template for data to be generated')
   .option('-c, --count [count]', 'The number of elements to create, defaults to 1', 1)
+  .option('-j, --json', 'Format the output as valid JSON, defaults to false', true)
   .parse(process.argv);
 
 var template;
@@ -21,12 +22,16 @@ try {
   return console.log(error);
 }
 
+function format(source, asJSON) {
+  return asJSON ? JSON.stringify(source, null, 2) : source;
+}
+
 if (program.count > 1) {
   var output = [];
   for (var i = 0; i < program.count; i++) {
     output.push(createData(template));
   }
-  return console.log(output);
+  return console.log(format(output, program.json));
 } else {
-  return console.log(createData(template));
+  return console.log(format(createData(template), program.json));
 }
